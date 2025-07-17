7159c610b1a99939cf16a2c7bfa9765ab1c0b0d7
walid <houiderwalid@gmail.com>
2025-07-17 10:51:44 +0100

    add more files

---
+++ nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    runtimeConfig: {
+        public: {
+            apiBaseURL: process.env.API_BASE_URL,
+            recaptchaSiteKey: process.env.RECAPTCHA_SITE_KEY,
+        }
+    },
+    modules:[
+        '@pinia/nuxt',
+        'pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/i18n',
+        '@nuxt/test-utils/module'
+    ],
+    i18n: {
+        compilation: {
+            strictMessage: false
+        },
+        locales: [
+            {
+                code: 'ar',
+                file: 'ar/index.js',
+                dir: 'rtl'
+            },
+            {
+                code: 'en',
+                file: 'en/index.js',
+                dir: 'ltr'
+            },
+            {
+                code: 'fr',
+                file: 'fr/index.js',
+                dir: 'ltr'
+            }
+        ],
+        lazy: true,
+        defaultLocale: 'en',
+        strategy: 'prefix',
+    }
+})
